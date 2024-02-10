11466f9a4f6da4963bbc4bb3e862db8f08569951
iliass bargache <ibargache6@gmail.com>
2024-02-10 13:54:26 +0100

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,12 @@
-# models/__init__.py
+#!/usr/bin/env python3
+"""
+models module documentation
+
+storage is a singleton to FileStorage
+and reload objects to file.json
+"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
