15abc17da5a059f9865ad26306be9b1f64db8e04
LewisJassy <jassylewis70@gmail.com>
2024-02-10 12:58:25 +0300

    add models and test folder

---
+++ models/base_model.py
@@ -0,0 +1,23 @@
+import uuid #Universally Unique Identifier
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self): #initializes the instance with a unique ID, creation timestamp
+        # and update timestamp
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+    def __str__(self): # provide human readable string representation of the instance
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
+    
