eef1fcd8f9fcae3949c9d99670de7cbe8234a133
LewisJassy <jassylewis70@gmail.com>
2024-02-11 09:46:46 +0300

    update init

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 # models/__init__.py
+"""this is the doc string for models module
+"""
+
+
+from models.engine import file_storage
+
+storage = file_storage.FileStorage()
+storage.reload()
