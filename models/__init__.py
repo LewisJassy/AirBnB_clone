50f7b3b759d4d0605f79a019c3b8ff1707cdbde9
LewisJassy <jassylewis70@gmail.com>
2024-02-11 09:53:38 +0300

    update the models

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-# models/__init__.py
-"""this is the doc string for models module
-"""
+"""Import FileStorage and read existing data into `storage` variable"""
 
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
-from models.engine import file_storage
+class_dict = {
+    'BaseModel': BaseModel,
+    'State': State,
+    'City': City,
+    'Amenity': Amenity,
+    'Place': Place,
+    'Review': Review,
+    'User': User
+}
 
-storage = file_storage.FileStorage()
+storage = FileStorage()
 storage.reload()
