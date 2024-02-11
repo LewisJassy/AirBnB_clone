e3932e0c75b5e1b8d1edf93d858f8441d538ab95
LewisJassy <jassylewis70@gmail.com>
2024-02-11 09:22:10 +0300

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,60 @@
-import uuid #Universally Unique Identifier
-from datetime import datetime
+#!/usr/bin/python3
+"""This module is the base model for use in all other models
+"""
 
 
-class BaseModel:
-    """Basemodel class for  data models."""
-    def __init__(self): #initializes the instance with a unique ID, creation timestamp
-        # and update timestamp
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+import datetime
+import models
+import uuid
 
-    def __str__(self): 
+
+class BaseModel():
+    """base model parent class for all other classes
+       used in project
+    """
+
+    def __init__(self, *args, **kwargs):
+        """init method for base class used in instantiation
         """
-        provide human readable string representation of the instance
+        if len(kwargs) >= 1:
+            self.set_from_dict(**kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """custom str method for str and print
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        builder = "["
+        builder += str(self.__class__.__name__) + '] ('
+        builder += str(self.id) + ') ' + str(self.__dict__)
+        return builder
 
     def save(self):
+        """save method used for updating class so updated_at changes
         """
-        Updates the update timestamp of the instance to the current time.
-        """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
-        Converts the instance into a dictionary.
+        returns the dictionary of our instance
         """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        return obj_dict
+        temp_d = self.__dict__.copy()
+        temp_d['__class__'] = self.__class__.__name__
+        temp_d['created_at'] = self.created_at.isoformat()
+        temp_d['updated_at'] = self.updated_at.isoformat()
+        return temp_d
+
+    def set_from_dict(self, **kwargs):
+        """sets attributes from dictionary
+        """
+        for (k, v) in kwargs.items():
+            if k in ('created_at', 'updated_at'):
+                self.__dict__[k] = datetime.datetime\
+                                           .strptime(v,
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.__dict__[k] = v
