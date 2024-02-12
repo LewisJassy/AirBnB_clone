7483dc8d21fc465a407c0d3e97eb51a7e0b1f273
LewisJassy <jassylewis70@gmail.com>
2024-02-12 16:06:40 +0300

    update the base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,61 @@
-import uuid #Universally Unique Identifier
+#!/usr/bin/python3
+"""BaseModel class"""
+
+import models
+import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Basemodel class for  data models."""
-    def __init__(self): #initializes the instance with a unique ID, creation timestamp
-        # and update timestamp
+    """BaseModel class to be inherited by other classes"""
+    def __init__(self, *args, **kwargs):
+        """__init__ method & instantiation of class Basemodel"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        for name, value in kwargs.items():
+            """searches through dict for keys"""
+            if name == "__class__":
+                continue
+            setattr(self, name, value)
+        if "id" not in kwargs:
+            models.storage.new(self)
+
+    def __setattr__(self, name, value):
+        """Maintain correct types for non-string attributes while keeping
+        the attributes as public attributes.
+
+        Args:
+            name (str): name of attribute
+            value: value to associate with `name`
+
+        Raises:
+            AttributeError: If value cannot be parsed into correct format
+        """
+        if name in ['created_at', 'updated_at']:
+            if isinstance(value, str):
+                try:
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                except ValueError:
+                    raise AttributeError("Invalid value: ({}) for name: ({})"
+                                         .format(value, name))
+        super().__setattr__(name, value)
 
     def __str__(self):
-        """custom str method for str and print
-        """
-        builder = "["
-        builder += str(self.__class__.__name__) + '] ('
-        builder += str(self.id) + ') ' + str(self.__dict__)
-        return builder
+        """Format `self` for output"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """save method used for updating class so updated_at changes
-        """
-        self.updated_at = datetime.datetime.now()
+        """updates the public instance attribute updated_at"""
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """
-        returns the dictionary of our instance
-        """
-        temp_d = self.__dict__.copy()
-        temp_d['__class__'] = self.__class__.__name__
-        temp_d['created_at'] = self.created_at.isoformat()
-        temp_d['updated_at'] = self.updated_at.isoformat()
-        return temp_d
-
-    def set_from_dict(self, **kwargs):
-        """sets attributes from dictionary
-        """
-        for (k, v) in kwargs.items():
-            if k in ('created_at', 'updated_at'):
-                self.__dict__[k] = datetime.datetime\
-                                           .strptime(v,
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            else:
-                self.__dict__[k] = v
+        """returns a dictionary containing all key/value pairs of __dict__"""
+        d = {}
+        d.update(self.__dict__)
+        d['created_at'] = d['created_at'].isoformat()
+        d['updated_at'] = d['updated_at'].isoformat()
+        d['__class__'] = self.__class__.__name__
+        return d
